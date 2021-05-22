8c583d35ffde80bfeae2c152c13271dfcefaf2f9
junheeyoon <enteryjh@ajou.ac.kr>
2021-05-22 16:05:31 +0900

    ide 변경(인텔리제이 -> vs code 통합)

--- src/test/java/com/backend/demo/demo/DemoApplicationTests.java
+++ src/test/java/com/backend/demo/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.backend;
+package com.backend.demo.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
